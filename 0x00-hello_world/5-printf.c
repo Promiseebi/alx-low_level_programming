19b2fab8c31969b98faba67f828e9cffcbb47c62
Promiseebi <miscus24@gmail.com>
2023-02-16 14:44:36 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fuction for prinf task
+ *
+ * Return: 0 when ok
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
